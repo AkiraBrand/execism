5ba04e20de221b8bf884aaf800b8c84fee23b845
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-08-21 08:40:08 -0600

    complete hello world and two-fer

---
+++ hello-world/hello_world.rb
@@ -0,0 +1,11 @@
+class HelloWorld
+attr_reader :hello
+
+
+#this is a class method, not an instance method. use these
+#in exism they use class methods
+  def self.hello
+    "Hello, World!"
+  end
+
+end
